46ef22635881d96c8d4c12d625ecb60996b74da7
DmitryHr <dmitry.hramenkov@gmail.com>
2016-12-06 11:50:46 +0300

    create classes/MainClass.class

---
+++ proj/EduJava/src/main/java/MainClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Alcom on 06.12.2016.
+ */
+public class MainClass {
+    public static void main(String [] arg){
+        TextForPrint tfp = new TextForPrint();
+        tfp.prText();
+    }
+}
